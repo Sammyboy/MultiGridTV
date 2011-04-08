7ea94e24ef73795490eb99aa4a3c9703cf9be604
Thomas Jakobi <thomas.jakobi@partout.info>
2011-04-08 15:51:55 +0200

    PHx modifier update
    Changes: docid in the modifier parameter (useful in ditto
    templates)

--- phx-modifier/gridisnotempty.phx.php
+++ phx-modifier/gridisnotempty.phx.php
@@ -1,5 +1,14 @@
 <?php
-$tvOutput = $modx->getTemplateVarOutput(array($options));
-$tvOutput = trim($tvOutput[$options]);
+/*  
+ * description: check whether the grid tv is not empty
+ * usage: [+phx:gridisnotempty=`tvname|docid`:then=``:else=`+]
+ * tvname = name of the grid tv
+ * docid = id of the document with the grid tv to use i.e. in ditto (defaults to current document)
+ */
+
+$options = explode('|', $options);
+$options[1] = isset($options[1]) ? $options[1] : $modx->documentIdentifier;
+$tvOutput = $modx->getTemplateVarOutput(array($options[0]), $options[1]);
+$tvOutput = trim($tvOutput[$options[0]]);
 $condition[] = intval($tvOutput != '[]' && $tvOutput != '');
 ?>
